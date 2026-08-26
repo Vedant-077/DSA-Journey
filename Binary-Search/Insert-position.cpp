2d11df824a74eadbc570611be0777d6e345abb0a
Vedant-077 <khatingvedant2004@gmail.com>
2026-08-26 23:34:00 +0530

    added Search Inseert Position Problem

---
+++ Binary-Search/Insert-position.cpp
@@ -0,0 +1,24 @@
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+      int low=0;
+      int high=nums.size()-1;
+      int ans=nums.size();
+      while(low<=high)
+      {
+        int mid=(low+high)/2;
+        if(target<=nums[mid]){
+        ans=mid;
+        high=mid-1;}
+        else
+        {
+         low=mid+1;
+        }
+      }
+
+  
+   return ans; }
+};
